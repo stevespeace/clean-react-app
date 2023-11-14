28ddb307eb5ddac4f49f4617d1b52834a58650b9
Steven Nguyen <steven08nguyen@gmail.com>
2023-11-13 16:52:15 -0800

    Today i learned how to code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import cyberlion from "./assets/CyberLions-8521.png"
 
 function App() {
   return (
     <div className="app">
-      <p>Hello World!</p>
+
+    <div className="outerdiv">
+
+      <img alt="CyberLions" src={cyberlion} />
+
+     <div className="textcontainer">
+       <h1>Hi!</h1>
+      <p>CyberLions!</p>
+      </div>
+    </div>
+
+
     </div>
   );
 }
